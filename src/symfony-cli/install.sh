e55ef4ab55cf74f075b6a521a7f2b8d25fbe411e
Shyim <github@shyim.de>
2022-11-05 23:11:35 +0000

    add symfony-cli installer

---
+++ src/symfony-cli/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+# check curl is installed and install it if missing
+if ! command -v curl &> /dev/null
+then
+    sudo apt-get install -y wget
+fi
+
+curl -sS https://get.symfony.com/cli/installer | sudo bash -s -- --install-dir=/usr/bin
