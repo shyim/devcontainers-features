93225da256f6b678c2bc21a038452c1d7c6dec35
Shyim <github@shyim.de>
2022-11-05 23:13:29 +0000

    add workflows

--- src/symfony-cli/install.sh
+++ src/symfony-cli/install.sh
@@ -5,7 +5,7 @@ set -e
 # check curl is installed and install it if missing
 if ! command -v curl &> /dev/null
 then
-    sudo apt-get install -y wget
+    sudo apt-get install -y curl
 fi
 
 curl -sS https://get.symfony.com/cli/installer | sudo bash -s -- --install-dir=/usr/bin
